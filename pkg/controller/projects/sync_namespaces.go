eda68bc6fd6a4327b7437b0d1e18472145c7694d
Bogdan Petrea <bogdan@presslabs.com>
2018-05-16 14:36:50 +0300

    Add label when creating namespace and better error handling.

--- pkg/controller/projects/sync_namespaces.go
+++ pkg/controller/projects/sync_namespaces.go
@@ -10,19 +10,30 @@ package projects
 import (
 	"github.com/golang/glog"
 
-	projectsApi "github.com/presslabs/dashboard/pkg/apis/projects/v1alpha1"
+	projects "github.com/presslabs/dashboard/pkg/apis/projects/v1alpha1"
 
-	coreapi "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 )
 
-func (c *Controller) syncNamespaces(project *projectsApi.Project) error {
+func (c *Controller) syncNamespaces(project *projects.Project) error {
 	glog.Infof("Syncing namespace for project %s", project.ObjectMeta.Name)
 
-	nsSpec := coreapi.Namespace{ObjectMeta: metav1.ObjectMeta{Name: project.ObjectMeta.Name}}
-	_, err := c.KubeClient.CoreV1().Namespaces().Create(&nsSpec)
+	ns := core.Namespace{
+		ObjectMeta: meta.ObjectMeta{
+			Name: project.ObjectMeta.Name,
+			Labels: labels.Set{
+				"dashboard.presslabs.com/project": project.ObjectMeta.Name,
+			},
+		},
+	}
+	_, err := c.KubeClient.CoreV1().Namespaces().Create(&ns)
 
-	glog.Infof("Created namespace for %s", project.ObjectMeta.Name)
-
+	if err != nil {
+		glog.Errorf("Error while creating namespace for %s: %v", project.ObjectMeta.Name, err)
+	} else {
+		glog.Infof("Created namespace for %s", project.ObjectMeta.Name)
+	}
 	return err
 }
